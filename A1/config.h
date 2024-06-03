0582890a3af2700d1e39542179aaa941b3f23a0e
kklipspringer <mennobrandt@protonmail.com>
2024-06-04 01:34:31 +0930

    A1,A2, No A3

---
+++ A1/config.h
@@ -0,0 +1,9 @@
+#define QLEN 6               /* size of request queue */
+#define MAX_HTTP_REQ_SIZE 2048 /* handle HTTP request up to 2KB */
+#define MAX_HTTP_RESPONSE_SIZE 2048 /* handle HTTP responses up to 2KB */
+// I modified this constant, to be 256. 
+#define MAX_HEADER_LENGTH 256  /* maximum length for HTTP headers */
+
+#define DEFAULT_PORT 8080    /* default port for server listening socket */
+
+#define RESOURCE_PATH "/public"  /* location of http files */
